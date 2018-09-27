3f1f38e0de3c44a05de10624b44f66e84d07e102
antoneville <antoneville@hotmail.com>
2018-09-27 12:48:41 +0100

    Complete - with Tests

--- Sphere.cs
+++ Sphere.cs
@@ -31,14 +31,28 @@ namespace C_Lab_Three_Interfaces
 
         static void Main()
         {
+            //Collection
             var objects = new [] { new Sphere { Radius = 4.2 }, 
                                    new Sphere { Radius = 6.3},
+                                   new Sphere { Radius = 8.7},
+                                   new Sphere { Radius = 3.2},
+                                   new Sphere { Radius = 9.1}
             };
 
+            //Testing Object 1.ToString()
             Console.WriteLine(objects[0]);
+            //Testing Object 1 calcVol method
             Console.WriteLine(objects[0].calcVol(objects[1]));
+            Console.WriteLine(objects[2]);
+            //Testing Setter Method
+            objects[2].Radius = 5.2;
+            //Checking to see if the setter method worked
+            Console.WriteLine(objects[2]);
+            //Checking to see if the getter method works
+            Console.WriteLine(objects[0].Radius);
+            Console.WriteLine(objects[3].calcVol(objects[4]));
         }
 
     }
-
+    
 }
